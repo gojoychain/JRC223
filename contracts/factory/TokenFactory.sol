e2463c2743330104cd3bb40d44518e7c91e3dbe4
Deric Walintukan <deric.walintukan@gmail.com>
2019-02-13 14:18:18 +0700

    Add token factory and safe math

---
+++ contracts/factory/TokenFactory.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.5.4;
+
+import "../token/PreMintedToken.sol";
+
+contract TokenFactory {
+    event PreMintedTokenCreated(
+        address indexed tokenAddress,
+        string name,
+        string symbol,
+        uint8 decimals,
+        uint256 totalSupply,
+        address owner
+    );
+
+    function createPreMintedToken(
+        string name,
+        string symbol,
+        uint8 decimals,
+        uint256 totalSupply,
+        address owner)
+        external
+        returns (PreMintedToken preMintToken)
+    {
+        PreMintedToken token = new PreMintedToken(name, symbol, decimals, totalSupply, owner);
+        emit PreMintedTokenCreated(address(token), name, symbol, decimals, totalSupply, owner);
+        return token;
+    }
+}
